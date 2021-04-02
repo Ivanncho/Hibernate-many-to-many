58d41750b2cafe7a5145bdabbafc95d3aabeecaf
Ivan <Ivan@TF0551>
2021-04-02 14:00:42 +0200

    Saving the students

--- hb-05-many-to-many/src/com/luv2code/hibernate/demo/CreateCourseAndStudentsDemo.java
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/CreateCourseAndStudentsDemo.java
@@ -30,11 +30,35 @@ public class CreateCourseAndStudentsDemo {
 			session.beginTransaction();
 			
 			//create a course
-			Course theCourse = new Course("Spring Boot for beginners");
+			Course theCourse = new Course("How to be a millionaire");
+			Course theCourse1 = new Course("1000 steps to success");
+			Course theCourse2 = new Course("Motivation is the way");
 			
 			
 			//save the course
+			System.out.println("\nSaving the course...");
 			session.save(theCourse);
+			session.save(theCourse1);
+			session.save(theCourse2);
+			System.out.println("Saved course: " + theCourse);
+			
+			//create the student
+			Student student1 = new Student("John","Doe","john.doe@email.com");
+			Student student2 = new Student("Jane","Doe","jane.doe@email.com");
+			Student student3 = new Student("Erik","Donalds","eDonalds@email.com");
+			//add students to the course
+			System.out.println("\nAdding student to courses");
+			theCourse.addStudent(student1);
+			theCourse1.addStudent(student2);
+			theCourse2.addStudent(student3);
+			theCourse.addStudent(student2);
+			theCourse2.addStudent(student1);
+			//save the students
+			System.out.println("\nSaving the students:" + "\n" + student1 + "\n" + student2 + "\n" + student3);
+			session.save(student1);
+			session.save(student2);
+			session.save(student3);
+			
 			
 			//commit transaction
 			session.getTransaction().commit();
